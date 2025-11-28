3fe1781e5d0d06fec7a3aec6b7670416f32afa62
c.bokkerink <c.bokkerink@toollive.com>
2025-11-28 16:26:31 +0100

    adding task title form

--- src/app/features/task/task.component.ts
+++ src/app/features/task/task.component.ts
@@ -1,12 +1,13 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Task } from '../../core/model/task';
 import { TaskService } from '../../core/service/task.service';
 
 @Component({
   selector: 'app-task',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ReactiveFormsModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.less'
 })
@@ -15,13 +16,22 @@ export class TaskComponent implements OnInit, OnDestroy {
   taskService: TaskService = inject(TaskService);
   tasks$: Observable<Task[]> = this.taskService.tasks$;
 
+  taskForm = new FormGroup({
+    name: new FormControl()
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  addTask(title: string): void {
-    this.taskService.addTask(title);
+  addTask(): void {
+    const form = this.taskForm.get('name')
+    if (form?.value) {
+      const title = form?.value
+      this.taskService.addTask(title);
+      form.setValue('')
+    }
   }
 
   ngOnDestroy(): void {
